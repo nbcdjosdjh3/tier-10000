032f0d7643d66178dd3ee2bb7042d853bcf940cd
nbcdjosdjh3 <gaotianmasaka70@gmail.com>
2026-05-25 22:05:52 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'tier-list-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './cropper.min.css',
+  './cropper.min.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
